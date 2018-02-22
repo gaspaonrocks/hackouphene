ea21bb0316e8d9101d3a05f930a1aa025ef65fc5
gaspaonrocks <gr.sacha.glgm@gmail.com>
2018-02-22 22:01:43 +0100

    added spotifyconfig

---
+++ src/app/config/spotifyApi.ts
@@ -0,0 +1,7 @@
+export default {
+    authorizeUrl: 'https://accounts.spotify.com/authorize',
+    clientId: '619bb4b421c648e1bc85039fa553097a',
+    redirectUri: 'http://localhost:4200/search',
+    responseType: 'token',
+    state: '123456'
+}
