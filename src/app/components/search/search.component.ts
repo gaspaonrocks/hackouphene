3029519b32c4eced6645b67f2519899897a00dc3
gaspaonrocks <gr.sacha.glgm@gmail.com>
2018-02-22 22:50:36 +0100

    spotify api query working !

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -9,15 +9,15 @@ import { ImplicitGrantService } from '../../services/implicit-grant.service';
 })
 export class SearchComponent implements OnInit {
   private authenticated: boolean;
-  public hashParams: Array<string>;
+  public hashParams: object;
   private categories: Array<string> = ['artist', 'song', 'album', 'genre'];
 
   constructor(private implicitService: ImplicitGrantService) {
     this.authenticated = false;
-    this.hashParams = [];
+    this.hashParams = {};
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   implicitGrantFlow(data) {
     console.log(data, 'it works');
@@ -25,18 +25,29 @@ export class SearchComponent implements OnInit {
   }
 
   nothingForNow() {
-    console.log(this.getHashParams());
+    this.hashParams = this.getHashParams();
+    console.log(this.hashParams);
     console.log('to be implemented...');
+    if (this.hashParams !== {}) { this.authenticated = true }
+    console.log(this.authenticated);
   }
 
   getHashParams() {
     const hashParams = {};
     let e;
     const r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
+      q = window.location.hash.substring(1);
+    while (e = r.exec(q)) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
     }
     return hashParams;
+  }
+
+  onSubmit(data) {
+    let submitCategory = encodeURIComponent(data.select);
+    let submitQuery = encodeURIComponent(data.query);
+    return this.implicitService
+      .querySpotifyApi({ submitCategory, submitQuery }, this.hashParams.access_token)
+      .subscribe(data => console.log(data));
   }
 }
--- src/app/services/implicit-grant.service.ts
+++ src/app/services/implicit-grant.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 
-import spotifyConfig from '../config/spotifyApi';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Access-Control-Allow-Origin': '*'
-  })
-};
+const spotifyConfig = {
+  authorizeUrl: 'https://accounts.spotify.com/authorize',
+  clientId: '619bb4b421c648e1bc85039fa553097a',
+  redirectUri: 'http://localhost:4200/search',
+  responseType: 'token',
+  state: '123456'
+}
 
 @Injectable()
 export class ImplicitGrantService {
@@ -22,9 +22,19 @@ export class ImplicitGrantService {
     '&state=' +
     encodeURIComponent(spotifyConfig.state);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getCredentialsToken() {
     return window.location = this.targetUrl;
+  }
+
+  querySpotifyApi(query, accessToken) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin': '*',
+        'Authorization': 'Bearer ' + accessToken
+      })
+    };
+    return this.http.get('https://api.spotify.com/v1/search?q=' + query.submitQuery + '&type=' + query.submitCategory, httpOptions)
   }
 }
